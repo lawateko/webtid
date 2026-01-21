8123bd930da5f58edd4b7fe4b9aae999bbfac0fa
lawaTeko <lawateko@gmail.com>
2026-01-21 16:36:06 +0900

    membuat tabel khusus untuk data dosen

--- myContent/dosen.php
+++ myContent/dosen.php
@@ -1,3 +1,49 @@
-<h2 class="dosen title">Dosen</h2>
-<p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, autem accusamus temporibus aut placeat neque dolore a aliquam cum ipsa fuga, minima praesentium facilis fugiat odio quis reiciendis ducimus ad ex sint illum. Ex voluptas quisquam fugiat reprehenderit obcaecati, omnis odio sequi alias similique nam praesentium harum animi nisi provident.</p>
-<img src="../cover.jpg" alt="">
+<div class="dosen-container container mt-3">
+    <div class="row g-4">
+        <div class="col-lg-4 col-md-5 col-sm-12">
+            <div class="card shadow-sm p-3">
+                <form>
+                    <div class="mb-4">
+                        <label for="#" class="form-label fs-5">NID</label>
+                        <input type="number" class="form-control border-3 p-2" id="#" required>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="#" class="form-label fs-5">Nama</label>
+                        <input type="text" class="form-control border-3 p-2" id="#" required>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="#" class="form-label fs-5">Alamat</label>
+                        <input type="text" class="form-control border-3 p-2" id="#" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-lg">Kirim</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="col-lg-8 col-md-7 col-sm-12">
+            <div class="card shadow-sm p-3">
+                <p class="fs-4 fw-bold border-bottom">DATA DOSEN</p>
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover fs-5">
+                        <thead class="table-dark">
+                            <tr>
+                                <th scope="col">NID</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Alamat</th>                        
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <td>230230403</td>
+                                <td>Ramdan Ope</td>
+                                <td>Ambon</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
