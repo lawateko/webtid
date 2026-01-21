a9da1f7f38a040178742ac294adb3d1f7dcb51eb
lawaTeko <lawateko@gmail.com>
2026-01-21 16:36:23 +0900

    hanya merubah sedikit hal disini

--- myContent/mahasiswa.php
+++ myContent/mahasiswa.php
@@ -4,18 +4,18 @@
             <div class="card shadow-sm p-3">
                 <form>
                     <div class="mb-4">
-                        <label for="#" class="form-label fs-5">Nama</label>
-                        <input type="text" class="form-control border-3 p-2" placeholder="Mr. Polytron" id="#" required>
-                    </div>
-
-                    <div class="mb-4">
                         <label for="#" class="form-label fs-5">NIM</label>
-                        <input type="number" class="form-control border-3 p-2" placeholder="240101011" id="#" required>
+                        <input type="number" class="form-control border-3 p-2" id="#" required>
                     </div>
 
                     <div class="mb-4">
-                        <label for="#" class="form-label fs-5">Email</label>
-                        <input type="text" class="form-control border-3 p-2" placeholder="example@gmail.com" id="#" required>
+                        <label for="#" class="form-label fs-5">Nama</label>
+                        <input type="text" class="form-control border-3 p-2" id="#" required>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="#" class="form-label fs-5">Alamat</label>
+                        <input type="text" class="form-control border-3 p-2" id="#" required>
                     </div>
 
                     <div class="mb-4">
@@ -43,41 +43,18 @@
                     <table class="table table-striped table-hover fs-5">
                         <thead class="table-dark">
                             <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Nama</th>
                                 <th scope="col">NIM</th>
-                                <th scope="col">Email</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Alamat</th>
                                 <th scope="col">Jurusan</th>
                             </tr>
                         </thead>
                         <tbody class="table-group-divider">
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Theo Lawalata</td>
-                                <td>2401011115</td>
-                                <td>lawateko@gmail.com</td>
+                                <td>240101115</td>
+                                <td>Fanuel Lawalata</td>
+                                <td>Kairatu</td>
                                 <td>Teknik Informatika</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Juan Wattimena</td>
-                                <td>2401011112</td>
-                                <td>wattimena@gmail.com</td>
-                                <td>Teknik Informatika</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Gabriel Haumahu</td>
-                                <td>2401011117</td>
-                                <td>haumahugabriel@gmail.com</td>
-                                <td>Sistem Informasi</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>Rizky Pratama</td>
-                                <td>2401011119</td>
-                                <td>prtamarzky@gmail.com</td>
-                                <td>Desain Komunikasi Visual</td>
                             </tr>
                         </tbody>
                     </table>
