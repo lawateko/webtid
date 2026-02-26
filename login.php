6e6057dd17901f9a1fcb8dcbd5adfd1cc1e99c41
lawaTeko <lawateko@gmail.com>
2026-02-26 21:58:39 +0900

    membuat halaman login

--- login.php
+++ login.php
@@ -1,15 +1,28 @@
 <?php
+
 require_once "config/koneksi.php";
 
-// if (isset($_POST["login"])){
-//     $username = $_POST["username"];
-//     $password = $_POST["password"];
+if (isset($_POST["login"])) {
 
-//     $result = $con->prepare("SELECT * FROM user WHERE username = '$username'");
-//     $result->execute([':username' => $username]);
-//     $user = $result->fetch(PDO::FETCH_ASSOC);
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-// }
+    $stmt = $con->prepare("SELECT * FROM user WHERE email = :email");
+    $stmt->execute(['email' => $email]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (password_verify($password, $user["password"])) {
+
+        header("Location: index.php");
+        exit;
+    } else {
+        echo "<script>
+            alert('Email/password salah, coba lagi!');
+            window.location.href = 'login.php';
+          </script>";
+    }
+}
 
 ?>
 
@@ -25,21 +38,21 @@ require_once "config/koneksi.php";
 
 <body>
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
-        <div class="col-md-4 mx-auto mt-5">
+        <div class="col-md-4 mx-auto ">
             <div class="card">
                 <div class="card-header fs-5 fw-bold text-center">LOGIN</div>
                 <div class="card-body">
                     <form action="" method="POST">
                         <div class="mb-3">
                             <label class="form-label fw-semibold" for="">Email</label>
-                            <input name="username" class="form-control" type="email" required>
+                            <input name="email" class="form-control" type="email" placeholder="emailkamu@gmail.com/@yahoo.com" required>
                         </div>
                         <div class="mb-3">
                             <label class="form-label fw-semibold" for="">Password</label>
                             <input name="password" class="form-control" type="password" required>
                         </div>
                         <div class="mb-3">
-                            <button class="btn btn-primary w-100 fw-semibold" type="submit" name="login">Login</button>
+                            <button class="btn btn-primary w-100 fw-semibold" type="submit" name="login">LOG IN</button>
                         </div>
                     </form>
                 </div>
