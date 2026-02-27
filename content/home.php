54aa6e7ad0edc0c0eceecdb416066df3a2cfc71f
lawaTeko <lawateko@gmail.com>
2026-02-27 15:26:28 +0900

    mengambil data nama user dan juga role. Dan juga menambahkan pesan selamat datang dengan di akhiri nama user yang login

--- content/home.php
+++ content/home.php
@@ -1,10 +1,15 @@
+<?php
+$nama_user = isset($_SESSION["user_nama"]) ? $_SESSION["user_nama"] : "User";
+$role = isset($_SESSION["user_role"]) ? $_SESSION["user_role"] : "";
+?>
+
 <div class="centered-jumbo lh-lg">
     <div class="jumbotron">
-        <h1 class="display-4 fw-bold">Hai yang di sana!</h1>
+        <h1 class="display-4 fw-bold">Hai, Selamat datang <?= htmlspecialchars($nama_user) ?>!</h1>
         <p class="lead fw-semibold">- Semua informasi tersedia dalam satu website yang mudah diakses.</p>
         <hr class="my-4">
         <p class="lead">
-            <a class="btn btn-primary btn-lg" href="#" role="button">Ayo mulai!</a>
+            <a class="btn btn-primary btn-lg" href="index.php?page=mahasiswa" role="button">Ayo mulai!</a>
         </p>
     </div>
 </div>
