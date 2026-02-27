a50888e82fa27e2137840fc1185c32adf580a3ac
lawaTeko <lawateko@gmail.com>
2026-02-27 22:05:37 +0900

    mengubah sedikit pada bagian jumbotron

--- content/home.php
+++ content/home.php
@@ -6,7 +6,7 @@ $role = isset($_SESSION["user_role"]) ? $_SESSION["user_role"] : "";
 <div class="centered-jumbo lh-lg">
     <div class="jumbotron">
         <h1 class="display-4 fw-bold">Hai, Selamat datang <?= htmlspecialchars($nama_user) ?>!</h1>
-        <p class="lead fw-semibold">- Semua informasi tersedia dalam satu website yang mudah diakses.</p>
+        <p class="lead fw-semibold">- Semua informasi tersedia dalam satu website💙!</p>
         <hr class="my-4">
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="index.php?page=mahasiswa" role="button">Ayo mulai!</a>
